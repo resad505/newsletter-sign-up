53f23752bf9b05681fbea6d0909f16ef37fc9822
resad505 <resadrules8@gmail.com>
2026-09-11 15:58:34 +0400

    feat: implement newsletter subscription success state and update layout styling

---
+++ script.js
@@ -0,0 +1,8 @@
+const submit_btn = document.querySelector("#submit")
+const success_msg = document.querySelector("#success")
+function submit_click(e) {
+    success_msg.classList.remove("hidden")
+
+
+
+}
