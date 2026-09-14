c95026061cacd12b9f6fa1fa24449549d4980115
resad505 <resadrules8@gmail.com>
2026-09-14 22:40:49 +0400

    feat: add success template and implement form submission view toggle logic

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const submit_btn = document.querySelector("#submit")
 const success_msg = document.querySelector("#success")
-function submit_click(e) {
+const main = document.querySelector("main")
+function submit__click(e) {
+    e.preventDefault();
     success_msg.classList.remove("hidden")
-
-
+    main.classList.add("hidden")
+    console.log("Clicked")
+}
+function dismiss() {
+    success_msg.classList.add("hidden")
+    console.log("Dismissed")
+    main.classList.remove("hidden")
 
 }
