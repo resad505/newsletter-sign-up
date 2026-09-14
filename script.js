0d32b37a5ddea957751de2259804edcb2b33b7d6
resad505 <resadrules8@gmail.com>
2026-09-14 23:37:45 +0400

    feat: add responsive styling to success message and update visibility toggle classes

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const success_msg = document.querySelector("#success")
 const main = document.querySelector("main")
 function submit__click(e) {
     e.preventDefault();
-    success_msg.classList.remove("hidden")
-    main.classList.add("hidden")
+    success_msg.classList.remove("!hidden")
+    main.classList.add("!hidden")
     console.log("Clicked")
 }
 function dismiss() {
-    success_msg.classList.add("hidden")
+    success_msg.classList.add("!hidden")
     console.log("Dismissed")
-    main.classList.remove("hidden")
+    main.classList.remove("!hidden")
 
 }
